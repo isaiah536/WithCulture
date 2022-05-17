80a2db4c5248fd1cabaddf86e335732d4a59199f
여현서 <leez2864@gmail.com>
2022-05-17 12:22:46 +0900

    Initial commit

---
+++ app/src/main/java/com/example/withme/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.withme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashActivity extends AppCompatActivity {
+    private Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash);
+        handler = new Handler(Looper.getMainLooper());
+    }
+    @Override protected void onStart() {
+        super.onStart();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SplashActivity.this.beginLoginActivity();
+            }
+        }, 2000);
+    }
+
+    private void beginLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private void beginMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
