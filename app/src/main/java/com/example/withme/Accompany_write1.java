666881bb2af8a01a5429e46ee5c8b1de533c98c1
여현서 <leez2864@gmail.com>
2022-05-23 23:07:25 +0900

    0523 여현서3

---
+++ app/src/main/java/com/example/withme/Accompany_write1.java
@@ -0,0 +1,14 @@
+package com.example.withme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Accompany_write1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accompany_write1);
+    }
+}
