3f07446a9f8d9349b274480900b7381150807f44
asdf <kjs8909539@naver.com>
2022-05-25 21:33:43 +0900

    fixup! 전시회 더보기 + 명화 선택

---
+++ app/src/main/java/com/example/withme/Category_2.java
@@ -0,0 +1,14 @@
+package com.example.withme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Category_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_2);
+    }
+}
