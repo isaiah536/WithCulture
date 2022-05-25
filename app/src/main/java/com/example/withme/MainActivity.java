47bd49a826cc89558be6e6873fcb6b3e0af7dd2c
여현서 <leez2864@gmail.com>
2022-05-25 14:52:49 +0900

    0525 여현서

--- app/src/main/java/com/example/withme/MainActivity.java
+++ app/src/main/java/com/example/withme/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.withme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        TextView allAccompany = findViewById(R.id.textView8);
+        allAccompany.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this,Accompany.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView allCommunity = findViewById(R.id.textView10);
+        allCommunity.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this,CommunityActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public void onBackPressed() {
+        //super.onBackPressed();
     }
 }
