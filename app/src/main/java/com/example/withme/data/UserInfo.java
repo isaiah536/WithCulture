d45bbcfefe94abb4f42189e977992f2bac672abd
여현서 <leez2864@gmail.com>
2022-05-26 03:13:05 +0900

    0526 여현서 로그인/회원가입 Activity

--- app/src/main/java/com/example/withme/data/UserInfo.java
+++ app/src/main/java/com/example/withme/data/UserInfo.java
@@ -1,4 +1,45 @@
 package com.example.withme.data;
 
-public enum UserInfo {
+import java.util.ArrayList;
+
+public class UserInfo {
+    ArrayList<String> infoemail = new ArrayList<>();
+    ArrayList<String> infoall = new ArrayList<>();
+
+    public UserInfo(){
+        infoemail.add("1");
+        infoall.add("1"+"="+"0");
+    }
+
+    public void addEmail(String s){
+        infoemail.add(s);
+    }
+    public void addAll(String s){infoall.add(s);}
+    public String getEmail(int x){return infoemail.get(x);}
+    public String getAll(int x) {return infoall.get(x);}
+
+    public boolean empty(){
+        return infoemail.isEmpty();
+    }
+
+    public boolean findEmail(String s){
+        if(infoemail.contains(s)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean login(String email, String password){
+        infoall.add("1=0");
+        for (int i=0; i<infoall.size();i++){
+            String [] temp = infoall.get(i).split("=");
+            for(int j=0; j<temp.length; j++){
+                if(email.equals(temp[0])&&password.equals(temp[1])){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
