60ff755c6c767d372e4e8b189ba8b3f283598626
Name명 <>
2022-05-29 21:51:35 +0900

    채팅 추가

--- app/src/main/java/com/example/withme/Chat3Activity.java
+++ app/src/main/java/com/example/withme/Chat3Activity.java
@@ -2,7 +2,14 @@ package com.example.withme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Chat3Activity extends AppCompatActivity {
 
@@ -10,5 +17,14 @@ public class Chat3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat3);
+
+        Button close = (Button) findViewById(R.id.close_btn);
+        close.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Chat3Activity.this, Chat2Activity.class);
+            }
+        });
+
     }
 }
