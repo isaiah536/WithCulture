8e945e5bd1937b40758f929f04fb7e79bc82122c
여현서 <leez2864@gmail.com>
2022-05-31 12:22:54 +0900

    0531 여현서

--- app/src/main/java/com/example/withme/Accompany_write1.java
+++ app/src/main/java/com/example/withme/Accompany_write1.java
@@ -5,14 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class Accompany_write1 extends AppCompatActivity {
+    ImageView preset_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accompany_write1);
+
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        layoutParams.dimAmount = 0.8f;
+        getWindow().setAttributes(layoutParams);
+
+        preset_profile = findViewById(R.id.write_preset_profile);
+        preset_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Accompany_write1.this,Profile_preview.class);
+                startActivity(intent);
+            }
+        });
 
         ImageView back2 = findViewById(R.id.back2);
         back2.setOnClickListener(new View.OnClickListener() {
@@ -22,5 +38,8 @@ public class Accompany_write1 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
+
 }
---
+++ app/src/main/java/com/example/withme/Profile_preview.java
@@ -0,0 +1,48 @@
+package com.example.withme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class Profile_preview extends AppCompatActivity {
+    ImageView back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_preview);
+
+        Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        int width = (int) (display.getWidth() * 0.9); //Display 사이즈의 90% 각자 원하는 사이즈로 설정하여 사용
+        int height = (int) (display.getHeight() * 0.7);  //Display 사이즈의 90% 각자 원하는 사이즈로 설정하여 사용
+        getWindow().getAttributes().width = width;
+        getWindow().getAttributes().height = height;
+        back = findViewById(R.id.back6);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+
+    public boolean onTouchEvent(MotionEvent event){
+        if(event.getAction()==MotionEvent.ACTION_OUTSIDE){
+            return false;
+        }
+        return true;
+    }
+/*
+    public void onBackPressed(){
+        return;
+    }
+
+ */
+}
