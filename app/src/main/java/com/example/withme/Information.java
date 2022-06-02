d9a1de850ebbe554d708f3dddc390d4bab2c9361
여현서 <leez2864@gmail.com>
2022-06-02 13:28:53 +0900

    0602 여현서 13:28

--- app/src/main/java/com/example/withme/Information.java
+++ app/src/main/java/com/example/withme/Information.java
@@ -2,7 +2,13 @@ package com.example.withme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Information extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class Information extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+
+        Button button = findViewById(R.id.button10);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Information.this,Famous_painting_1.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView imageView = findViewById(R.id.imageView12);
+        imageView.setColorFilter(Color.parseColor("#aa808080"), PorterDuff.Mode.SRC_ATOP);
     }
 }
